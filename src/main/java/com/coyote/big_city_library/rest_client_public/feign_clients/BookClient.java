d3fc057b0234f42defcd1ba878529eb246a83261
Coyote-31 <coyotepouil@gmail.com>
2022-02-03 15:50:55 +0100

    🕹 Update FeignClient Mapping for /search to Post

--- src/main/java/com/coyote/big_city_library/rest_client_public/feign_clients/BookClient.java
+++ src/main/java/com/coyote/big_city_library/rest_client_public/feign_clients/BookClient.java
@@ -5,13 +5,13 @@ import java.util.List;
 import com.coyote.big_city_library.rest_client_public.dto.BookDto;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "BigCityLibrary-RestServer-book", url = "localhost:9001", path = "/api/books")
 public interface BookClient {
     
-    @GetMapping("/search")
+    @PostMapping("/search")
     List<BookDto> searchBooks(
         @RequestParam("bookTitle") String bookTitle,
         @RequestParam("authorName") String authorName,
